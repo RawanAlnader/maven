1de454860f485a7ffdfa7055b2b986fd1fab415e
jawad74 <gwadxx@gmail.com>
2021-08-23 18:15:37 +0300

    push files

---
+++ src/main/java/com/devops/maven/Main.java
@@ -0,0 +1,20 @@
+package com.devops.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        int num1=2,num2=3;
+        System.out.println( num1+"+"+num2+"="+sum(num1,num2) );
+    }
+
+	public int calculateSomething() {
+		return 0;
+	}
+	public static int sum(int a, int b){return a+b;}
+}
+
